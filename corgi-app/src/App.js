65d66a6d24b3b614484159baaa0f39976f2e91c6
warandstar <andreakim123@naver.com>
2021-01-16 10:30:53 -0800

    Add components from original project to react

--- corgi-app/src/App.js
+++ corgi-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="contents">
+      <div id="myCanvas" class="background-formatter">
+        <img src="./assets/penguin-blue.jpg" class="character" alt = "character-align" />
+      </div>
+      <div class="chat">chat</div>
     </div>
   );
 }
